9f00969a04e43d0b2a7d08cfa5b8d07a81ec85e9
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-02-20 17:11:40 +0600

    add protected route feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -11,6 +12,14 @@ function App() {
           {/* Define routes here */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/editor"
+            element={
+              <ProtectedRoute>
+                <h1>Code Editor</h1>
+              </ProtectedRoute>
+            }
+          />
           <Route path="/" element={<h1>Welcome to IDE-X</h1>} />
         </Routes>
       </div>
