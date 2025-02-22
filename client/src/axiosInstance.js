96ccaee53867479e40708da516ad68db17157124
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-02-23 01:42:06 +0600

    add request interceptor

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -6,4 +6,18 @@ const axiosInstance = axios.create({
   withCredentials: true, // Include credentials (e.g., cookies) if needed
 });
 
+// Add a request interceptor to include the JWT token in the headers
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); // Get the token from localStorage
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`; // Add the token to the Authorization header
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
