a5b7264d83d0f02bc6cf32a8ca5b638de975f1ba
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-04-17 15:37:44 +0600

    change callback url and refactor code

--- server/config/passport.js
+++ server/config/passport.js
@@ -5,15 +5,29 @@ import { Strategy as GitHubStrategy } from 'passport-github2';
 // Load User Model
 import User from '../models/user.model.js';
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
 passport.use(
   new GitHubStrategy(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'api/auth/github/callback',
+      callbackURL: `${process.env.CLIENT_URL}/api/auth/github/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
+        // console.log('GitHub Profile:', profile); // remove later
         // Check if user exists
         let user = await User.findOne({ githubId: profile.id });
         if (user) {
@@ -23,7 +37,10 @@ passport.use(
         // Create a new user
         user = new User({
           name: profile.displayName,
-          email: profile.emails[0].value,
+          email:
+            profile.emails && profile.emails.length > 0
+              ? profile.emails[0].value
+              : `${profile.username}@github.com`,
           githubId: profile.id,
           avatar: profile.photos[0].value,
         });
@@ -37,15 +54,4 @@ passport.use(
   )
 );
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err, null);
-  }
-});
+export default passport;
